1468e9579d6d83f9d1627cd8b3078e73da081501
Vipin Singh <svipin174@gmail.com>
2022-03-06 11:02:29 +0530

    inintial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = require("axios");
+var url = 'https://jsonplaceholder.typicode.com/todos/1';
+axios_1.default.get(url).then(function (response) {
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var finished = todo.completed;
+    logData(id, title, finished);
+});
+var logData = function (id, title, finished) {
+    console.log("\n    The todo with ID: ".concat(id, "\n    Has a title of: ").concat(title, "\n    Is it finished?: ").concat(finished, "\n  "));
+};
